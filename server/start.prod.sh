aaa41de1a0dad7348bfa2ac927473cf82d8f5122
yahyatoumi <80495767+yahyatoumi@users.noreply.github.com>
2024-10-18 23:11:38 +0100

    done

--- server/start.prod.sh
+++ server/start.prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Wait for the database to be available (optional, if needed)
-# sleep 10
+sleep 10
 
 # Apply migrations
 
 # Start the application with hot reload
-dotnet watch run
+dotnet bin/Debug/net8.0/server.dll
