5a0b32282c44ad926455c589e81ab0fea97770f6
yahyatoumi <80495767+yahyatoumi@users.noreply.github.com>
2024-10-20 12:45:06 +0100

    Unit tests done

--- cypress/cypress/e2e/ticket_management.cy.js
+++ cypress/cypress/e2e/ticket_management.cy.js
@@ -6,16 +6,43 @@ describe('Ticket Management', () => {
 
     it('should create a new ticket', () => {
         // Click on the "Add New Ticket" button
-        cy.get('[data-testid="add-new-ticket-btn"]').click();
+        cy.get('[data-testid="add-new-ticket-btn"]').first().click();
 
         // Fill in the ticket form
-        // cy.get('input[name="description"]').type('New Ticket from Cypress');
-        // cy.get('select[name="status"]').select('Open');
+        cy.get('textarea[name="description"]').type('New Ticket from Cypress');
+        cy.get('select[name="status"]').select('Open');
 
-        // // Submit the form
-        // cy.get('[data-testid="submit-ticket-btn"]').click();
+        // Submit the form
+        cy.get('[data-testid="new-ticket-submit-btn"]').click();
 
-        // // Verify the ticket is added
-        // cy.get('.ticket-list').should('contain', 'New Ticket from Cypress');
+        // Verify the ticket is added
+        cy.get('[data-testid="tickets-list"]').should('contain', 'New Ticket from Cypress');
     });
+
+
+    it('should update an existing ticket', () => {
+        cy.get('tr td').contains('span', 'Update').click();
+
+        // Update the ticket's description
+        cy.get('textarea[name="description"]').clear().type('Updated Ticket from Cypress');
+        cy.get('select[name="status"]').select('Open');
+
+
+        // // Submit the update
+        cy.get('[data-testid="update-ticket-submit-btn"]').click();
+
+        // // Verify the update
+        cy.get('[data-testid="tickets-list"]').should('contain', 'Updated Ticket from Cypress');
+    });
+
+    it('should delete a ticket', () => {
+        cy.get('tr td').contains('span', 'Delete').click();
+
+
+        cy.get('[data-testid="delete-ticket-btn"]').first().click();
+
+        cy.get('[data-testid="tickets-list"]').should('not.contain', 'Updated Ticket from Cypress');
+
+    });
+
 });
